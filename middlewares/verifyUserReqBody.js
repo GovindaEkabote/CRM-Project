7753ce3095728285c464b34e0905c5db8d8c0c7d
GovindaEkbote <govindaekbote7@gmail.com>
2025-10-24 13:40:48 +0530

    Done: create middleware when admin update emp profile it will check if is it proper userStatus and userType

--- middlewares/verifyUserReqBody.js
+++ middlewares/verifyUserReqBody.js
@@ -59,6 +59,49 @@ const validateUserRequestBody = async (req, res, next) => {
   next();
 };
 
+const validateUserStatusAndUserType = (req, res, next) => {
+  try {
+    const { userType, userStatus } = req.body;
+
+    const validUserTypes = [
+      constant.userType.employee,
+      constant.userType.ADMIN,
+      constant.userType.IT_SUPPORT,
+    ];
+
+    if (userType && !validUserTypes.includes(userType.toUpperCase())) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Invalid userType. Allowed values: EMPLOYEE | IT_SUPPORT | ADMIN",
+      });
+    }
+
+    const validUserStatuses = [
+      constant.userStatus.pending,
+      constant.userStatus.approved,
+      constant.userStatus.blocked,
+    ];
+
+    if (userStatus && !validUserStatuses.includes(userStatus.toUpperCase())) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Invalid userStatus. Allowed values: PENDING | APPROVED | BLOCKED",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error validating user type/status:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error while validating user fields.",
+    });
+  }
+};
+
 module.exports = {
   validateUserReqBody: validateUserRequestBody,
+  validateUserStatusAndUserType:validateUserStatusAndUserType
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const Users = require('../controller/user.controller')
-const {verifyToken,isAdmin} = require('../middlewares/authjwt')
+const express = require("express");
+const Users = require("../controller/user.controller");
+const { verifyToken, isAdmin } = require("../middlewares/authjwt");
+const verifyUserReqBody = require("../middlewares/verifyUserReqBody");
 const route = express.Router();
-
 
 /*
 GET /hrm/api/v1/users?page=2&limit=5
 GET /hrm/api/v1/users?userType=EMPLOYEE&userStatus=APPROVED&page=1&limit=10
 GET /hrm/api/v1/users?search=ravis
 */
-route.get('/users',verifyToken,isAdmin,Users.findAll)
-route.get('/me',verifyToken,Users.user)
-route.get('/employee/:empId',verifyToken,isAdmin,Users.user)
-route.get('/employee/update/:userId',verifyToken,isAdmin,Users.updateUser)
+route.get("/users", verifyToken, isAdmin, Users.findAll);
+route.get("/me", verifyToken, Users.user);
+route.get("/employee/:empId", verifyToken, isAdmin, Users.user);
+route.put(
+  "/employee/update/:userId",
+  verifyToken,
+  isAdmin,
+  verifyUserReqBody.validateUserStatusAndUserType,
+  Users.updateUser
+);
 
 module.exports = route;
