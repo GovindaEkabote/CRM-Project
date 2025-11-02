b05b83e3d52f0f157e1b51cdd5be9d26dd1eb968
GovindaEkbote <govindaekbote7@gmail.com>
2025-11-02 22:29:43 +0530

    Done: Get All your comments

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -1,18 +1,16 @@
 const Ticket = require("../models/ticket.model");
-const TicketComment = require("../models/ticket.model");
+const TicketComment = require("../models/comment.model");
 const constant = require("../utils/constant");
 
 exports.addComment = async (req, res) => {
   try {
     const { ticketId } = req.params;
     const { comment } = req.body;
-    const userId = req.user._id || req.user.id;
-    const role = req.user.userType;
 
     if (!comment || comment.trim() === "") {
       return res.status(400).json({
         success: false,
-        message: "Comment cannot be empty.",
+        message: "Comment cannot be empty",
       });
     }
 
@@ -24,28 +22,70 @@ exports.addComment = async (req, res) => {
       });
     }
 
-    if (
-      !role === constant.userType.ADMIN ||
+    const userId = req.user._id || req.user.id;
+    const role = req.user.userType.toUpperCase();
+
+    // ✅ Access rules
+    const isAllowed =
+      role === constant.userType.ADMIN ||
       role === constant.userType.IT_SUPPORT ||
-      ticket.reporter.toString() === userId.toString()
-    ) {
+      ticket.reporter.toString() === userId.toString();
+
+    if (!isAllowed) {
       return res.status(403).json({
         success: false,
-        message: "Not authorized to comment on this ticket.",
+        message: "Not authorized to comment",
       });
     }
+
     const newComment = await TicketComment.create({
       ticket: ticketId,
       user: userId,
       comment,
     });
-    return res.status(201).json({
+
+    res.status(201).json({
       success: true,
-      message: "Comment added successfully.",
+      message: "Comment added",
       data: newComment,
     });
   } catch (error) {
-    console.error("Add Comment Error:", err);
-    res.status(500).json({ success: false, message: "Server Error" });
+    console.error("Add Comment Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+exports.getComments = async (req, res) => {
+  try {
+    const { ticketId } = req.params;
+    const userId = req.user._id || req.user.id;
+    const role = req.user.userType;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      return res.status(404).json({
+        success: false,
+        message: "Ticket not found",
+      });
+    }
+
+    const comments = await TicketComment.find({ ticket: ticketId })
+      .populate("user", "fullName email userType")
+      .sort({ createdAt: 1 });
+
+    return res.status(200).json({
+      success: true,
+      count: comments.length,
+      data: comments,
+    });
+  } catch (error) {
+    console.log("Get Comments Error", error);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
   }
 };
--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const comment = require("../controller/comment.controller");
 const constant = require("../utils/constant");
 const { verifyToken, isAdmin } = require("../middlewares/authjwt");
+const authorizeRoles = require("../middlewares/authorizeRole");
 
-
-router.post('/ticket/:ticketId', verifyToken,comment.addComment);
-
-
+router.post("/ticket/comments/:ticketId", verifyToken, comment.addComment);
+router.get(
+  "/ticket/comments/:ticketId",
+  verifyToken,
+  authorizeRoles(
+    constant.userType.IT_SUPPORT ||
+    constant.userType.ADMIN ||
+    constant.userType.employee
+  ),
+  comment.getComments
+);
 
 module.exports = router;
